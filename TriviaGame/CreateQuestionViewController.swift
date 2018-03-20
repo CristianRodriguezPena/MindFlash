dfb0d9d6194b193e09ec9f3a9971e934a2995a04
CristianRodriguezPena <cristianrodriguez@Interapts-MacBook-Pro-141.local>
2018-03-20 16:34:50 -0400

    working product

--- TriviaGame/CreateQuestionViewController.swift
+++ TriviaGame/CreateQuestionViewController.swift
@@ -9,16 +9,30 @@
 import UIKit
 
 class CreateQuestionViewController: UIViewController {
-
+    @IBOutlet weak var cancel: UIButton!
+    @IBOutlet weak var add: UIButton!
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var optionATextField: UITextField!
+    @IBOutlet weak var optionBTextField: UITextField!
+    @IBOutlet weak var optionCTextField: UITextField!
+    @IBOutlet weak var optionDTextField: UITextField!
+    @IBOutlet weak var correctOption: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        cancel.layer.cornerRadius = cancel.frame.height * 0.15
+        add.layer.cornerRadius = add.frame.height * 0.15
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        if questionTextField.text != "" && optionATextField.text != "" && optionBTextField.text != "" && optionCTextField.text != "" && optionDTextField.text != "" {
+            GuessViewController.questionList.append(Question.init(question: questionTextField.text!, options: [optionATextField.text!, optionBTextField.text!, optionCTextField.text!, optionDTextField.text!], correctAnswer: correctOption.selectedSegmentIndex + 1))
+        }
     }
     
     @IBAction func cancelButtonTapped(_ sender: Any) {
--- TriviaGame/GuessViewController.swift
+++ TriviaGame/GuessViewController.swift
@@ -16,9 +16,17 @@ class GuessViewController: UIViewController {
     @IBOutlet weak var addAQuestion: UIButton!
     @IBOutlet weak var reset: UIButton!
     @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
     
-    var questionList = [Question]()
-    var currentCorrectAnswer :String!
+    public static var questionList = [Question]()
+    var currentCorrectAnswer :String! {
+        return GuessViewController.questionList.filter({$0.question == question.text!})[0].correctAnswer
+    }
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score : \(score)"
+        }
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -30,7 +38,7 @@ class GuessViewController: UIViewController {
         addAQuestion.layer.cornerRadius = addAQuestion.frame.height * 0.15
         reset.layer.cornerRadius = reset.frame.height * 0.15
         addPreloadedQuestions()
-        showQuestion(randomNumber(questionList.filter {$0.answeredCorrectly == nil}.count))
+        showQuestion(randomNumber(GuessViewController.questionList.filter {$0.answeredCorrectly == nil}.count))
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,18 +51,36 @@ class GuessViewController: UIViewController {
     }
     
     func addPreloadedQuestions() {
-        questionList.append(Question.init(question: "Who was the 20th president of the United States?", options: ["Grover Cleveand","James A. Garfield","Theodore Roosevelt","Andrew Johnson"], correctAnswer: 2))
-        questionList.append(Question.init(question: "Which bird is often associated with delivering babies?", options: ["Owl","Bald Eagle","Hummingbird","Stork"], correctAnswer: 4))
-        questionList.append(Question.init(question: "Which country has the longest land border?", options: ["Russia","Canada","China","India"], correctAnswer: 3))
-        questionList.append(Question.init(question: "Who wrote The Little Mermaid?", options: ["Hans Christian Andersen","","","William Shakespeare"], correctAnswer: 1))
-        questionList.append(Question.init(question: "Who is the current supreme leader of North Korea?", options: ["Ri Sol-ju","Kim Jong Un","kim jong il","kim jong nam"], correctAnswer: 2))
-        questionList.append(Question.init(question: "What is the favorite food of the Teenage Mutant Ninja Turtles?", options: ["Pizza","Tacos","Bread","Cheese Burgers"], correctAnswer: 1))
-        questionList.append(Question.init(question: "According to the popular Christmas song, Frosty the Snowman, what are the snowman's eyes made out of?", options: ["Coal","Carrots","Wood","Snow"], correctAnswer: 1))
-        questionList.append(Question.init(question: "What is the most common blood type in humans?", options: ["A-","O","O+","Red Blood"], correctAnswer: 3))
-        questionList.append(Question.init(question: "What is the national sport of Japan?", options: ["Soccer","American Football","Sumo","Cricket"], correctAnswer: 3))
-        questionList.append(Question.init(question: "Mexican tortillas were originally made from the grain of which plant?", options: ["Mushroom","Wheat","Potatoes","Corn"], correctAnswer: 4))
+        GuessViewController.questionList.append(Question.init(question: "Who was the 20th president of the United States?", options: ["Grover Cleveand","James A. Garfield","Theodore Roosevelt","Andrew Johnson"], correctAnswer: 2))
+        GuessViewController.questionList.append(Question.init(question: "Which bird is often associated with delivering babies?", options: ["Owl","Bald Eagle","Hummingbird","Stork"], correctAnswer: 4))
+        GuessViewController.questionList.append(Question.init(question: "Which country has the longest land border?", options: ["Russia","Canada","China","India"], correctAnswer: 3))
+        GuessViewController.questionList.append(Question.init(question: "Who wrote The Little Mermaid?", options: ["Hans Christian Andersen","J.K. Rowling","Mark Twain","William Shakespeare"], correctAnswer: 1))
+        GuessViewController.questionList.append(Question.init(question: "Who is the current supreme leader of North Korea?", options: ["Ri Sol-ju","Kim Jong Un","Kim Jong Il","Kim Jong Nam"], correctAnswer: 2))
+        GuessViewController.questionList.append(Question.init(question: "What is the favorite food of the Teenage Mutant Ninja Turtles?", options: ["Pizza","Tacos","Bread","Cheese Burgers"], correctAnswer: 1))
+        GuessViewController.questionList.append(Question.init(question: "According to the popular Christmas song, Frosty the Snowman, what are the snowman's eyes made out of?", options: ["Coal","Carrots","Wood","Snow"], correctAnswer: 1))
+        GuessViewController.questionList.append(Question.init(question: "What is the most common blood type in humans?", options: ["A-","O","O+","Red Blood"], correctAnswer: 3))
+        GuessViewController.questionList.append(Question.init(question: "What is the national sport of Japan?", options: ["Soccer","American Football","Sumo","Cricket"], correctAnswer: 3))
+        GuessViewController.questionList.append(Question.init(question: "Mexican tortillas were originally made from the grain of which plant?", options: ["Mushroom","Wheat","Potatoes","Corn"], correctAnswer: 4))
     }
-    func changeOption(_ number : Int, _ text : String) {
+    
+    func optionManager(_ number : Int, _ text : String?) -> String? {
+
+        
+        if text == nil {
+            switch number {
+            case 1:
+                return optionOne.titleLabel?.text!
+            case 2:
+                return optionTwo.titleLabel?.text!
+            case 3:
+                return optionThree.titleLabel?.text!
+            case 4:
+                return optionFour.titleLabel?.text!
+            default:
+                return "Something when wrong checking the options"
+            }
+        }
+        
         switch number {
         case 1:
             optionOne.setTitle(text, for: .normal)
@@ -67,20 +93,17 @@ class GuessViewController: UIViewController {
         default:
             print("Something when wrong changing the options")
         }
+        return nil
     }
     
     func showQuestion(_ questionNumber: Int) {
-        let currentQuestion = questionList[questionNumber]
+        let currentQuestion = GuessViewController.questionList.filter({$0.answeredCorrectly == nil})[questionNumber]
         question.text = currentQuestion.question
-        var usedOptions = [String]()
-        var optionNumber = 1
-        while usedOptions.count < 4 {
-            let randomOption = currentQuestion.options[randomNumber(4)]
-            if !usedOptions.contains(randomOption) {
-                changeOption(optionNumber, randomOption)
-                usedOptions.append(randomOption)
-                optionNumber += 1
-            }
+        var avaliableOptions = currentQuestion.options
+        for i in 1...4 {
+            let randomOption = randomNumber(avaliableOptions.count)
+            optionManager(i, avaliableOptions[randomOption])
+            avaliableOptions.remove(at: randomOption)
         }
     }
     
@@ -88,16 +111,18 @@ class GuessViewController: UIViewController {
         return Int(arc4random_uniform(UInt32(max)))
     }
     
-    @IBAction func optionOneTapped(_ sender: UIButton) {
-    }
-    
-    @IBAction func optionTwoTapped(_ sender: UIButton) {
-    }
-    
-    @IBAction func optionThreeTapped(_ sender: UIButton) {
-    }
-    
-    @IBAction func optionFourTapped(_ sender: UIButton) {
+    @IBAction func optionTapped(_ sender: UIButton) {
+        GuessViewController.questionList.filter({$0.answeredCorrectly == nil}).filter({$0.correctAnswer == currentCorrectAnswer})[0].answeredCorrectly = false
+        
+        if optionManager(sender.tag, nil) == currentCorrectAnswer {
+            GuessViewController.questionList.filter({$0.correctAnswer == currentCorrectAnswer})[0].answeredCorrectly = true
+            score += 1
+        }
+        if GuessViewController.questionList.filter({$0.answeredCorrectly == nil}).count == 0 {
+            exit(0)
+        }
+        
+        showQuestion(randomNumber(GuessViewController.questionList.filter {$0.answeredCorrectly == nil}.count))
     }
     
 }
